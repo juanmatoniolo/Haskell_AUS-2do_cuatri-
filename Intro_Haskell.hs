19ab8f21d7c43fd1bca1dca36aa753fdfe2c8e51
JunmaToniolo <juanmatoniolo2@gmail.com>
2022-08-29 16:20:38 -0300

    2

--- Intro_Haskell.hs
+++ Intro_Haskell.hs
@@ -8,4 +8,13 @@ doblesmas x y = x * 2 + y *2
 
 -- podemos llamar funciones dentro de funciones: --
 -- hacemos el doble de X  y el doble de X y el doble de Y para  luego sumamos
-dobledoble x y = doble x + (doblesmas x y)
+dobledoble x y = doble x + (doblesmas x y) 
+
+-- Ejemplo de funcion 
+
+menores x = if x > 100
+    then x 
+    else x *2
+
+-- Listas Condicionales --
+funcion1 x = [x*2 | x <- [1..x]]
